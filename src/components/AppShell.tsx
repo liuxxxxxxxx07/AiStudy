6d828a8289838a9eac63872c26fc0c3ff5345f8a
liuxxxxxxxx07 <3154651386@qq.com>
2026-06-26 02:24:26 +0800

    更新

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -498,7 +498,7 @@ export default function AppShell({ user, onLogout }: { user: Record<string, unkn
         />
       )}
 
-      <main className="flex-1 flex flex-col min-w-0">
+      <main className="flex-1 flex flex-col min-w-0 pb-6">
         <header className="flex items-center justify-between px-5 py-2.5 border-b border-divider bg-background">
           {!sidebarOpen && (
             <button
